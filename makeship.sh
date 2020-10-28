f6a4ac44f73a7b4c4d40b5bed5a01a4b28f03905
Bjorn Engsig <bjorn.engsig@oracle.com>
2020-10-28 17:27:44 +0000

    Minor update

--- makeship.sh
+++ makeship.sh
@@ -1,4 +1,9 @@
 # make the shipfile
+#
+# Changes
+#
+# bengsig  19-oct-2020 Add man pages
+# bengsig  09-sep-2020 Creation
 
 name=`rwloadsim -x '$mute:99' --version | sed -r -n 's/.*Release ([0-9]+\.[0-9]+\.[0-9]+).*/\1/ p'`
 longname=`rwloadsim -x '$mute:99' --version | sed -r -n 's/.*Release ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).*/\1/ p'`
@@ -16,6 +21,6 @@ fi
 rm -f $tgzfile
 cwd=`pwd`
 (cd; tar -cf $cwd/rwl/admin/vim.tar .vim/ftdetect/rws.vim .vim/ftdetect/rwl.vim .vim/syntax/rwl.vim)
-tar -zcf $tgzfile --exclude-from=tar.exclude bin rwl/admin rwl/demo rwl/public rwl/doc
+tar -zcf $tgzfile --exclude-from=tar.exclude bin rwl/man rwl/admin rwl/demo rwl/public rwl/doc/*.md
 
 echo Created $tgzfile containing a $banner release
