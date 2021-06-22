1aa2ff67027a202f01235835fcf721c5ce01ffd6
Bjørn Kisbye Engsig <bjorn.engsig@oracle.com>
2021-06-22 12:55:46 +0000

    Master is now 2.3.6

--- src/rwl.h
+++ src/rwl.h
@@ -1679,8 +1679,8 @@ void rwlechooff(int);
 
 #define RWL_VERSION_MAJOR 2
 #define RWL_VERSION_MINOR 3
-#define RWL_VERSION_RELEASE 5
-#define RWL_VERSION_TEXT "Production" RWL_EXTRA_VERSION_TEXT
+#define RWL_VERSION_RELEASE 6
+#define RWL_VERSION_TEXT "Development" RWL_EXTRA_VERSION_TEXT
 #define RWL_VERSION_DATE // undef to not include compile date 
 extern ub4 rwlpatch;
 
