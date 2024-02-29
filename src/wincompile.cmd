647ca7852448141d631fedfb0c85969ff3825637
Bjorn Kisbye Engsig <bjorn.engsig@oracle.com>
2024-02-29 16:08:46 +0100

    Changes to make and ship on Windows

--- src/wincompile.cmd
+++ src/wincompile.cmd
@@ -1,5 +1,19 @@
+rem RWP*Load Simulator
+rem
+rem Copyright (c) 2024 Oracle Corporation
+rem Licensed under the Universal Permissive License v 1.0
+rem as shown at https://oss.oracle.com/licenses/upl/
+rem
+rem Windows: modify rwlpatch.c and run nmake
+rem
+rem History
+rem
+rem bengsig  29-feb-2024  Creation
+rem
+
 set /p rwlpatch<rwlpatch.txt
 set /a rwlpatch+=1
 echo %rwlpatch% > rwlpatch.txt
 echo unsigned int rwlpatch=%rwlpatch%; > rwlpatch.c
+mkdir winobj
 nmake /F Makefile.win
---
+++ winship.cmd
@@ -0,0 +1,19 @@
+rem RWP*Load Simulator
+rem
+rem Copyright (c) 2024 Oracle Corporation
+rem Licensed under the Universal Permissive License v 1.0
+rem as shown at https://oss.oracle.com/licenses/upl/
+rem
+rem Create the zip file for shipping on Windows
+rem
+rem History
+rem
+rem bengsig  29-feb-2024  Creation
+rem
+
+del rwloadsim-Windows-bin-3.1.1.zip
+
+7z a rwloadsim-Windows-bin-3.1.1.zip .\DISTRIBUTION.txt .\LICENSE.txt .\SECURITY.md .\CONTRIBUTING.md .\README.md .\CHANGELOG.md
+7z a rwloadsim-Windows-bin-3.1.1.zip admin demo public docs
+7z a rwloadsim-Windows-bin-3.1.1.zip bin\rwlerror.exe bin\rwloadsim.exe lib\rwlgenexec.obj lib\rwlgenobjects.lib lib\wingen.cmd
+7z l rwloadsim-Windows-bin-3.1.1.zip 
