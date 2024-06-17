a85e6fd5c763e82f6e5873eb6c35ecd89a10da18
Bjorn Kisbye Engsig <bjorn.engsig@oracle.com>
2024-06-17 16:08:27 +0000

    Allow different index partition count for mtit

--- oltp/mtit_drop.sql
+++ oltp/mtit_drop.sql
@@ -10,6 +10,7 @@ rem Name
 rem   mtit_drop.sql - Drop Max Throughput Insert Tranaction objects
 rem
 rem History
+rem   bengsig  17-jun-2024 - partitioned index rename
 rem   bengsig  10-jun-2024 - hcc
 rem   bengsig   9-sep-2023 - rename seq
 rem   bengsig  10-may-2023 - Creation
@@ -26,7 +27,7 @@ drop table aw_mtit_ix purge
 drop table aw_mtit_revix purge
 /
 
-drop table aw_mtit_ixp8 purge
+drop table aw_mtit_partix purge
 /
 
 drop table aw_mtit_revixempty purge
